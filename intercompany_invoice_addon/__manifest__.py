21c20617d6e4c9a0d250ac76279b6fe083cd1d06
Jos-dooit <103411981+Jos-dooit@users.noreply.github.com>
2023-02-23 11:53:28 +0100

    Add files via upload

--- intercompany_invoice_addon/__manifest__.py
+++ intercompany_invoice_addon/__manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://www.odoo.com",
     "license": "OEEL-1",
     "category": "Uncategorized",
-    "version": "15.0.1.0.0",
+    "version": "16.0.2.0.1",
     "depends": [
         "account",
     ],
